84819b700f8e3035498a1e2b1b929be16bc7b8e6
alikaakati <akaakati@blankstreet.com>
2025-07-23 06:14:00 +0300

    fix: use env variable values instead of hard coded urls

--- src/routes/etl.routes.ts
+++ src/routes/etl.routes.ts
@@ -23,8 +23,7 @@ router.post("/quickbooks", async (req, res) => {
     await financialApp.initialize();
 
     // Use default QuickBooks data URL
-    const quickBooksUrl =
-      "https://file.notion.so/f/f/341ba97f-38cd-4258-82a9-94cc0df05d13/3e13a193-001b-4fb8-80cf-ea40eb3c5682/Income_Statement_Company_1.json?table=block&id=22a353cd-1af1-8006-bd9c-d33ec0b1f7cb&spaceId=341ba97f-38cd-4258-82a9-94cc0df05d13&expirationTimestamp=1753221600000&signature=TAJ2Vz_Ko48vUXhWdS6npjCTFNAeEFkT5c7mPL5vlTw&downloadName=Income_Statement_Company_1.json";
+    const quickBooksUrl = process.env.QUICKBOOKS_URL || "";
 
     const result = await financialApp.processQuickBooksData(quickBooksUrl);
 
@@ -60,8 +59,7 @@ router.post("/rootfi", async (req, res) => {
     await financialApp.initialize();
 
     // Use default Rootfi data URL
-    const rootfiUrl =
-      "https://file.notion.so/f/f/341ba97f-38cd-4258-82a9-94cc0df05d13/fea332be-907b-483b-a798-57c305fa8e7a/Income_Statement_Company_2.json?table=block&id=22a353cd-1af1-800c-9be7-fb79305c06fd&spaceId=341ba97f-38cd-4258-82a9-94cc0df05d13&expirationTimestamp=1753221600000&signature=K-0moqniOb7PGpuQ8xIUbZj4CEA-tPJlC5Zg-WOfKtU&downloadName=Income_Statement_Company_2.json";
+    const rootfiUrl = process.env.ROOTFI_URL || "";
 
     const result = await financialApp.processRootfiData(rootfiUrl);
 
@@ -97,10 +95,8 @@ router.post("/all", async (req, res) => {
     await financialApp.initialize();
 
     // Use default data URLs
-    const quickBooksUrl =
-      "https://file.notion.so/f/f/341ba97f-38cd-4258-82a9-94cc0df05d13/3e13a193-001b-4fb8-80cf-ea40eb3c5682/Income_Statement_Company_1.json?table=block&id=22a353cd-1af1-8006-bd9c-d33ec0b1f7cb&spaceId=341ba97f-38cd-4258-82a9-94cc0df05d13&expirationTimestamp=1753221600000&signature=TAJ2Vz_Ko48vUXhWdS6npjCTFNAeEFkT5c7mPL5vlTw&downloadName=Income_Statement_Company_1.json";
-    const rootfiUrl =
-      "https://file.notion.so/f/f/341ba97f-38cd-4258-82a9-94cc0df05d13/fea332be-907b-483b-a798-57c305fa8e7a/Income_Statement_Company_2.json?table=block&id=22a353cd-1af1-800c-9be7-fb79305c06fd&spaceId=341ba97f-38cd-4258-82a9-94cc0df05d13&expirationTimestamp=1753221600000&signature=K-0moqniOb7PGpuQ8xIUbZj4CEA-tPJlC5Zg-WOfKtU&downloadName=Income_Statement_Company_2.json";
+    const quickBooksUrl = process.env.QUICKBOOKS_URL || "";
+    const rootfiUrl = process.env.ROOTFI_URL || "";
 
     const result = await financialApp.processAllData(quickBooksUrl, rootfiUrl);
 
